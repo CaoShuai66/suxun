6ea57c1f6af2196f3f9ad2e554556f0407b054c0
曹帅 <815417140@qq.com>
2025-11-12 09:51:07 +0800

    项目初始化

---
+++ main.js
@@ -0,0 +1,33 @@
+const { updateElectronApp } = require('update-electron-app')
+
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  updateElectronApp();
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
